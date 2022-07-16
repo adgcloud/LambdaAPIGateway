0618dc7a7360d466c2cfb69aa0912a063cb9f3a3
adgcloud <adgcloud@gmail.com>
2022-07-16 14:19:52 +0000

    added more comments

--- createSQS.py
+++ createSQS.py
@@ -3,6 +3,8 @@ import boto3
 #Create a variable to target the sqs service
 sqs = boto3.resource('sqs')
 
+#Using the Boto3 commands, create a sqs queue and call it 'MySQSQueue' and store it in a variable
 sqsqueue = sqs.create_queue(QueueName='MySQSQueue')
 
+#Print the atttributes of the newly created Queue
 print(sqsqueue)
